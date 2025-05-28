9be32308fd94c30fef58672164664b04bffd7d73
MOHAMED THANVEER <thanveerami612@gmail.com>
2025-05-28 14:33:57 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\SchoolController;
+use App\Http\Controllers\ClassController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\StudentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/school', [SchoolController::class, 'index'])->name('school');
+Route::get('/classes', [ClassController::class, 'index'])->name('classes');
+Route::get('/events', [EventController::class, 'index'])->name('events');
+Route::get('/students', [StudentController::class, 'index'])->name('students');
